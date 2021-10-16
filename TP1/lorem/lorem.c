a95f896a649e10785118218e72aab8a4a05b5338
Collette <maxime.collette2@etu.unistra.fr>
2021-10-16 16:46:16 +0200

    Exercice

---
+++ TP1/lorem/lorem.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void) {
+  printf("Lorem ipsum dolor sit"
+  " amet, consectetur adipiscing"
+  " elit, sed do eiu" "smod tempor \
+incididunt ut labore et dolore \
+magna aliqua.\n");
+  printf("Ut enim ad minim veniam, quis nostrud exercitation ");
+  printf("ullamco laboris nisi ut aliquip ex ea commodo ");
+  printf("consequat.\n");
+}
