403552512336fc70f61d1c105fcbb1946e00698c
Collette <maxime.collette2@etu.unistra.fr>
2021-11-16 13:04:46 +0100

    Exercice

--- algo/carte-reduction/reduc.c
+++ algo/carte-reduction/reduc.c
@@ -1,45 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
-// int iserdico(int *tab, int value, int *size)
-// {
-//     int deb = 0;
-//     int fin = *size;
-//     while (deb != fin)
-//     {
-//         int ite=(fin-deb)/2;
-//         if (tab[ite] < value)
-//         {
-//             fin=ite;
-//         }
-//         else if (tab[ite] == value)
-//         {
-//             return 0;
-//         }
-//         else
-//         {
-//             deb=ite;
-//         }
-//     }
-// }
-
 int main(void)
 {
     int N;
     scanf("%i", &N);
-    int cartes[N];
+    int present[1000000] = {0};
     int a;
     for (int i = 0; i < N; i++)
     {
         scanf("%i", &a);
-        for (int j = 0; j < i; j++)
+        if (present[a] == 1)
         {
-            if (a == cartes[j])
-            {
-                printf("%i\n", a);
-                return 0;
-            }
+            printf("%i\n", a);
+            return 0;
         }
-        cartes[i] = a;
+        present[a] = 1;
     }
     printf("-1\n");
 }
