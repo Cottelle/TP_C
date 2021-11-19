3e6ce090464a9885d3db7ed1b9eaf31458c7655f
Collette <maxime.collette2@etu.unistra.fr>
2021-11-19 14:33:09 +0100

    Exercice

---
+++ annales/latex-indent/fonction.h
@@ -0,0 +1,5 @@
+#include <stdio.h>
+
+
+void readline(FILE *file,char* buffer);
+/* lit simplement une ligne de de file (de longueur maximal 1000) et la sock dans le buffer de taille 1001 */
--- envoiegit.sh
+++ envoiegit.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+if [ $# -eq 1 ]
+then
+cd $1
+fi
 git add .
 git commit -m "Exercice"
 git push origin master
