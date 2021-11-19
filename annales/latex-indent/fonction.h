a498a42d7af9f67b9baa69aee21ac49e882e411a
Collette <maxime.collette2@etu.unistra.fr>
2021-11-19 14:33:34 +0100

    Exercice

--- annales/latex-indent/fonction.h
+++ annales/latex-indent/fonction.h
@@ -3,3 +3,5 @@
 
 void readline(FILE *file,char* buffer);
 /* lit simplement une ligne de de file (de longueur maximal 1000) et la sock dans le buffer de taille 1001 */
+
+int countpath(char *str,char )
