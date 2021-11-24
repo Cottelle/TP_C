734f45838190f80841474945e740ded06871bd1c
Collette <maxime.collette2@etu.unistra.fr>
2021-11-24 09:06:33 +0100

    Exercice

--- TP11/canary-malloc/ca_malloc.c
+++ TP11/canary-malloc/ca_malloc.c
@@ -4,44 +4,41 @@
 
 void *ca_malloc(int n)
 {
-    char *retour = malloc(n + 16);
+    char *retour = malloc(n + 16+4);
     if (retour == NULL)
     {
         return NULL;
-        for (int i = 0; i < n + 16; i++)
-        {
-            retour[i] = 'a';
-        }
     }
     for (int i = 0; i < 8; i++)
     {
-        retour[n + 16 - i - 1] = 'c';
+        retour[n + 20 - i - 1] = 'c';
+        retour[i+4] = 'c';
+
     }
-    // (long *)retour;
-    ((long *)retour)[0] =(long)n;
+    ((int *)retour)[0] =(int)n;
     (void *)retour;
-    return (retour + 8);
+    return (retour + 12);
+    
 }
 
 void ca_free(void *pointeur)
 {
     (void *)pointeur;
     pointeur = pointeur - 8;
-    long *recupnb;
-    long n = ((long *)pointeur)[0];
+    int n = ((int *)pointeur)[-1];
     // printf("%i", n);
     char *test = pointeur;
     for (int i = 0; i < 8; i++)
     {
         // printf("%c",test[n+16 - i - 1]);
-        if (test[n + 16 - i - 1] != 'c')
+        if (test[n + 16 - i - 1] != 'c'|| test[i]!='c')
         {
             fprintf(stderr, "Erreur vous avez fait n'importe quoi avec le pointeur que je vous ai passé, Signé Malloc");
-            free(pointeur);
+            free(pointeur-4);
             return;
         }
     }
-    free(pointeur);
+    free(pointeur-4);
     return;
 }
 
